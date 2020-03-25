d65b2ea1ff43023085874973eefb68f18cb1c0a2
John Clifford <johnclifford00@gmail.com>
2020-03-25 12:39:04 -0400

    refactored code and combined / seperated javascript functions for better logic

--- rgbgame.js
+++ rgbgame.js
@@ -6,43 +6,10 @@ let newButton = document.querySelector(".new");
 let colorDisplay = document.querySelector(".colorDisplay");
 let easy = document.querySelector("#easy");
 let hard = document.querySelector("#hard");
-let isEasy = false;
 let squareNumber = 6;
+let pickedColor = "";
 
 
-
-//assesses whether a user has selected the correct color and gives feedback or ends the game
-function gamePlay() {
-    for(i = 0; i < squareNumber; i++) {
-        squares[i].style.backgroundColor = colors[i];
-        squares[i].addEventListener("click", function() {
-            if(this.style.backgroundColor === pickedColor) {
-                tryAgainMessage.textContent = "Correct";
-                changeColors(pickedColor);
-                newButton.textContent = "Play Again?";
-            }
-            else {
-                this.style.backgroundColor = "#232323";
-                tryAgainMessage.textContent = "Try again";
-                newButton.textContent = "New Colors";
-            }
-        })
-    }
-}
-
-//changes all square colors and the banner at the top of the page to match the winning color
-function changeColors(color) {
-    h1.style.backgroundColor = color;
-    for(i = 0; i < squares.length; i++) {
-        squares[i].style.backgroundColor = color;
-    }
-}
-
-//Chooses one of the colors to be the winning color
-function pickWinningColor() {
-    let rand = Math.floor(Math.random() * colors.length);
-    return colors[rand];
-}
 
 //Chooses 6 random RGB colors and builds them into a string
 function pickAllColors(num) {
@@ -57,44 +24,68 @@ function randomRGB() {
     return Math.floor(Math.random() * 256);
 }
 
+//Chooses one of the colors to be the winning color
+function pickWinningColor() {
+    let rand = Math.floor(Math.random() * colors.length);
+    return colors[rand];
+}
 
-
-function freshGame() {
-    tryAgainMessage.textContent = "";
-    newButton.textContent = "New colors";
-    colors = []
-    if(isEasy === true) {
-        pickAllColors(3);
+//Checks if clicked square is the winning square and display winning message
+function checkIfWinner() {
+    if(this.style.backgroundColor === pickedColor) {
+        tryAgainMessage.textContent = "Correct";
+        changeColors(pickedColor);
+        newButton.textContent = "Play Again?";
     }
     else {
-        pickAllColors(6);
+        this.style.backgroundColor = "#232323";
+        tryAgainMessage.textContent = "Try again";
+        newButton.textContent = "New Colors";
     }
-    pickedColor = pickWinningColor();
-    colorDisplay.textContent = pickedColor;
-    changeColors("#232323");
-    gamePlay();
+}
 
+//changes all square colors and the banner at the top of the page to match the given color
+function changeColors(color) {
+    h1.style.backgroundColor = color;
+    for(i = 0; i < squareNumber; i++) {
+        squares[i].style.backgroundColor = color;
+    }
 }
 
 
-pickAllColors(6)
-let pickedColor = pickWinningColor();
-colorDisplay.textContent = pickedColor;
-gamePlay();
+function newGame() {
+    tryAgainMessage.textContent = "";
+    newButton.textContent = "New colors";
+    colors = [];
+    pickAllColors(squareNumber);
+    pickedColor = pickWinningColor();
+    colorDisplay.textContent = pickedColor;
+    changeColors("#232323");
+    for(i = 0; i < squareNumber; i++) {
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].addEventListener("click", checkIfWinner);
+    }
+
+}
 
 easy.addEventListener("click", function() {
-    isEasy = true;
+    changeColors("#232323");
     squareNumber = 3;
-    freshGame();
+    easy.classList.add("selected");
+    hard.classList.remove("selected");
+    newGame();
 })
 
 hard.addEventListener("click", function() {
-    isEasy = false;
     squareNumber = 6;
-    freshGame();
+    hard.classList.add("selected");
+    easy.classList.remove("selected");
+    newGame();
 })
 
 //listens for clicks on the button allowing users to start a new game or restart
 newButton.addEventListener("click", function() {
-    freshGame();
+    newGame();
 })
+
+newGame();
